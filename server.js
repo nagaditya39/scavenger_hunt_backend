97e75cdad62cf5f3c5b1dbd08b6f0b366e0c4117
nagaditya39 <nagaditya39@gmail.com>
2024-09-20 12:48:51 +0530

    cors bugifx removed wildcard

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: '*',
-}));
+app.use(cors({}));
 app.use(bodyParser.json());
 
 
